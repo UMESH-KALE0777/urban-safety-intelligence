279f2f04787d6d894fe906f56ed2a032208e48d0
Umesh  Kale <umesh.kale09192@gmail.com>
2026-04-07 22:00:23 +0530

    Delete src directory

--- src/data/data_loader.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_crime_data(path="data/raw/crime_dataset.csv"):
-    try:
-        df = pd.read_csv(path)
-        return df
-    except Exception as e:
-        print("Error loading data:", e)
-        return None
